370d9ec9cdb63c5ccbe0b7136ca8f0b784d1525e
jorgeheag <herreraajorgeh@gmail.com>
2022-07-12 11:36:18 -0500

    relations

--- server.js
+++ server.js
@@ -1,16 +1,38 @@
 const { app } = require('./app');
 
+//Models
+const {User} = require('./models/users.model');
+const {Reviews} = require('./models/reviews.model');
+const {Restaurants} = require('./models/restaurants.model');
+const {Orders} = require('./models/orders.model');
+
 //utils
-const { /*sequelize*/ } = require(/*database*/);
+const { db } = require('./Utils/data.base');
+const { Meals } = require('./models/meals.model');
 
 // Database authenticated
-sequelize
+db
 	.authenticate()
 	.then(() => console.log('Database authenticated'))
 	.catch(err => console.log(err));
 
+//Unit models relations
+User.hasMany(Orders, {foreignkey: 'UserId', through: 'mealId'});
+Orders.belongsTo(User);
+
+User.hasMany(Reviews, {foreignkey: 'UserId', through: 'restaurantId'});
+Reviews.belongsTo(User);
+
+Restaurants.hasMany(Reviews, {foreignkey: 'restaurantId', through: 'UserId'})
+Reviews.belongsTo(Restaurants);
+
+Meals.hasOne(Orders, {foreignkey: 'mealId', through: 'id'})
+Orders.belongsTo(Meals);
+
+//revisar las relaciones
+
 // Database synced with models' relations  
-sequelize
+db
 	.sync()
 	.then(() => console.log('Database synced'))
 	.catch(err => console.log(err));
