67bb900c9209de07ddf594ad2ba8c1fdcc148d42
jeremy <jeremyvallejo2016@gmail.com>
2022-07-13 15:59:07 -0500

    miercoles 1

--- midlewares/order.diddleware.js
+++ midlewares/order.diddleware.js
@@ -1,9 +1,23 @@
 // Models
 const { Meals } = require('../models/meals.model');
+const { Orders } = require('../models/orders.model');
 
 // Utils
 const { AppError } = require('../utils/appError');
 const { catchAsync } = require('../utils/catchAsync');
+
+const totalprice = catchAsync(async (req, res, next) => {
+	
+	const totalprice =  req.body * Orders.quantity
+
+	Orders.totalPrice= totalprice
+
+	req.totalPrice = totalPrice;
+	next();
+});
+
+
+
 
 const meatExist= catchAsync(async (req, res, next) => {
 	const { id } = req.params;
